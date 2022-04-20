57e5540811337f4ed05b4bd8b9d956e4decf3b47
Crowe7 <jacobocrowe@gmail.com>
2022-04-20 03:56:37 -0500

    added dotenv and set env variable needed to auth bot

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,9 +1,15 @@
 import { Client } from "discord.js"
+import 'dotenv/config'
+import express from 'express'
 
 console.log("Bot Starting...");
 
 const client = new Client({
     intents: []
 });
+
+const TOKEN = process.env.TOKEN
+
+client.login(TOKEN);
 
 console.log(client);
