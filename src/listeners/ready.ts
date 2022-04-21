f9c42b1a0443c037cf0862bff119ae298e7036e5
Crowe7 <jacobocrowe@gmail.com>
2022-04-21 01:48:04 -0500

    updated ready to used rest.put to place the command if it doesn't exist

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,10 +1,24 @@
 import { Client } from "discord.js";
+import {Commands} from '../AllCommands';
+import { REST } from '@discordjs/rest';
+import 'dotenv/config'
+import { Routes } from "discord-api-types/v10";
 
 export default (client: Client): void => {
     client.on("ready", async () => {
-        if(!client.user || !client.application) {
-            return;
-        }
+        if(!client.user || !client.application) {return}
+        const rest = new REST({ version: "9" }).setToken(
+            process.env.TOKEN as string
+        );
+        const commandData = Commands.map((command) => command.data.toJSON());
+
+        await rest.put(
+            Routes.applicationGuildCommands(
+                client.user.id,
+                process.env.SERVER_ID as string
+            ),
+            {body: commandData}
+        )
         console.log(`${client.user.username} is online`);
     });
 };
