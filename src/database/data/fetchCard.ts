94cace5e4adf11f154bc7615229db738e5434262
Crowe <81550296+Crowe7@users.noreply.github.com>
2022-06-10 17:19:53 -0500

    psudo code for fetching cards

---
+++ src/database/data/fetchCard.ts
@@ -0,0 +1,6 @@
+// When we fetch the card from the api we need to save all the cards we find
+// for each we save put those into a seperate array
+// use that array and use that to save the Card collection model 
+
+// so we save the card into the db as a card. then take the id of that same card and push that to an array
+// using that array to populate the CardCollection
---
+++ src/database/data/fetchCardFromDB.ts
@@ -0,0 +1,6 @@
+// when fetching a card from the db, maybe check if setname is being usedm if not just use that name to search the Card collection
+// if setname is being query the actual CardCollection to check for the cards specific name
+    // then check each card inside and see if the set matches
+    // if you find a match return that card info
+    // if no match just return the first card in the array itself.
+    
