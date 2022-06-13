324707b8d4be3111cfe896b67d3ff6b26e8474f4
Crowe <81550296+Crowe7@users.noreply.github.com>
2022-06-13 14:04:02 -0500

    added save cards to DB function

--- src/database/data/saveCardsToDB.ts
+++ src/database/data/saveCardsToDB.ts
@@ -4,3 +4,46 @@ import { saveCardCollection } from "../models/CardCollection";
 import { saveNonValidCard } from "../models/NoScryfallListing";
 
 
+ export const saveCardsToDB = async (cards: unknown[], name: string) => {
+    const cardArr: ObjectId[] = [];
+
+    if(!cards) {
+        await saveNonValidCard(name);
+        return
+    }
+    else {
+        cards.forEach( async (card) => {
+            if(isCard(card)) {
+                await saveCard(card.name, card.set, card);
+                let foundCard = await Card.findOne({ name: card.name }).exec();
+                cardArr.push(foundCard?._id as unknown as ObjectId);
+            }
+        });
+        if(isCard(cards[0])) {
+            await saveCardCollection(cards[0].name, cardArr);
+        }
+    }    
+}
+
+/*
+
+          const cardArr = []
+        await saveCard(fakeCard.name, fakeCard.set, fakeCard);
+
+        let card = await Card.findOne({ name: fakeCard.name }).exec();
+
+        cardArr.push(card?._id as unknown as ObjectId);
+
+        console.log(card)
+        await saveCardCollection(fakeCard.name, cardArr);
+*/
+
+/*
+
+        let card = await CardCollection.findOne({ name: fakeCard.name }).populate({path: 'sets', model: Card}).exec()
+        if (isCard(card?.sets[0])) {
+            console.log(card?.sets[0].details.name);
+        } else {
+            console.log('Not type card');
+        }
+*/
