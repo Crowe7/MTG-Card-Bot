9a9e28ed2b49e63bfb258fb7848610e14dd5113c
Crowe <81550296+Crowe7@users.noreply.github.com>
2022-06-13 14:41:01 -0500

    changed regex to match anywhere instead of beginning to match api

--- src/data/matchNameRegex.ts
+++ src/data/matchNameRegex.ts
@@ -1,5 +1,5 @@
 export const matchName = (word: string) => {
-    const match = new RegExp(`^${word}.*$`);
+    const match = new RegExp(`${word}.*$`);
     return match
 }
 // this might need to change to just allow the text to match ANYWHERE instead of just the start.
