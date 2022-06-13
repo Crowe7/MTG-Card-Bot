f4b5bd79a4762611d0563d450528ad3b0ab5f330
Crowe <81550296+Crowe7@users.noreply.github.com>
2022-06-13 10:15:59 -0500

    added every second cron to test implementation

--- src/cronjobs/cashResetCron.ts
+++
@@ -1,9 +0,0 @@
-import { CronJob } from 'cron'
-import { cacheReset } from './cacheReset';
-
-console.log('Before job instantiation');
-const job = new CronJob('0 0 0 * * 6', async () => {
-   await cacheReset();
-});
-console.log('Cache Cleared!');
-job.start();
