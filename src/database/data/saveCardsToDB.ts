432db48d6d8254b7cc7a026140fd1a2efdab5aff
Crowe <81550296+Crowe7@users.noreply.github.com>
2022-06-14 09:37:33 -0500

    fixed saveCardsToDB bug

--- src/database/data/saveCardsToDB.ts
+++ src/database/data/saveCardsToDB.ts
@@ -1,36 +1,39 @@
-import mongoose, { ObjectId } from "mongoose";
+import mongoose, { Document, ObjectId, Schema } from "mongoose";
 import { Card, isCard, saveCard } from "../models/Card";
 import { saveCardCollection } from "../models/CardCollection";
 import { saveNonValidCard } from "../models/NoScryfallListing";
+
+import { Types } from "mongoose"
+
 
 import 'dotenv/config'
 mongoose.connect(process.env.MONGO_URI as string);
 
 
  export const saveCardsToDB = async (cards: unknown[], name: string) => {
-    const cardArr: ObjectId[] = [];
+     //set to any because setting to ObjectId[] doesnt work;
+    let cardArr: any[] = [];
     if(cards.length === 0 || !cards) {
         await saveNonValidCard(name);
         return;
     }
     else {
-        cards.forEach( async (card) => {
+        for(const card of cards) {
             if(isCard(card)) {
                 await saveCard(card.name, card.set, card);
                 let foundCard = await Card.findOne({ name: card.name }).exec();
-                cardArr.push(foundCard?._id as unknown as ObjectId);
+                if(foundCard) {
+                    cardArr = [...cardArr,  foundCard?._id] ;
+                }
             }
-        });
-        if(isCard(cards[0])) {
-            await saveCardCollection(cards[0].name, cardArr);
-        }
+        };
+    }
+    if(isCard(cards[0]) && cardArr.length !== 0) {
+        console.log(cardArr)
+        await saveCardCollection(cards[0].name, cardArr);
     }    
 }
-
-saveCardsToDB([], "Crunt");
-
 /*
-
           const cardArr = []
         await saveCard(fakeCard.name, fakeCard.set, fakeCard);
 
