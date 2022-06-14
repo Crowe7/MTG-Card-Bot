f37cf3833a1c8622919be9740fc5b60d025a94d8
Crowe7 <jacobocrowe@gmail.com>
2022-06-13 22:59:15 -0500

    fixed bug with striptext

--- src/data/convertText.ts
+++ src/data/convertText.ts
@@ -1,15 +1,14 @@
 const reducer = (characters: string[]): string[] => {
     const reduced = characters.reduce( (filtered: string[], char) => {
-        if(char !== char.toUpperCase()) {
-            let lowercased = char.toLowerCase();
-            filtered.push(lowercased);
+    	let lowerchar = char.toLowerCase();
+        if(lowerchar !== char.toUpperCase()) {
+            filtered.push(lowerchar);
         }
         return filtered;
     }, []);
 
     return reduced;
 }
-
 export const stripAndForceLowerCase = (word: string) => {
     const splitText: string[] = word.split("");
     const finalText = reducer(splitText).join('');
