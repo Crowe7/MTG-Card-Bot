c766cb1a665cf1591c83d56542961596b9900fcf
Crowe <81550296+Crowe7@users.noreply.github.com>
2022-06-17 10:34:09 -0500

    fixed bug with scryfall not showing exact card matches

--- src/database/data/fetchCard.ts
+++ src/database/data/fetchCard.ts
@@ -1,10 +1,13 @@
+import { stripAndForceLowerCase } from "../../data/convertText";
+
 export const fetchCardAPI = async (name: string) => {
     const cardMatches = await fetch(`https://api.scryfall.com/cards/autocomplete?q=${name}`)
         .then(response => response.json());
 
-    const firstCardMatch:string = cardMatches.data[0];
+    const firstCardMatch:string = stripAndForceLowerCase(cardMatches.data[0]);
+    console.log(firstCardMatch);
 
-    const cards = await fetch(`https://api.scryfall.com/cards/search?q=+not%3Adigital+${firstCardMatch}&unique=prints&as=grid&order=released`)
+    const cards = await fetch(`https://api.scryfall.com/cards/search?q=+not%3Adigital+%21${firstCardMatch}&unique=prints&as=grid&order=released`)
         .then(res => res.json());
     return(cards.data)
 
