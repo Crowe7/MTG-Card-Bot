c182f316ee5a8f9faa6d29d9898aa0abe8e01636
Crowe <81550296+Crowe7@users.noreply.github.com>
2022-06-20 18:51:39 -0500

    added todo for new feature

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,31 @@ interactionCreate(client);
 
 client.login(TOKEN);
 
-// TODO SAVE CARDS DATABASE
+// TODO ADD COLLECTION COMMAND
+
+/*
+
+    WRITE SCHEMA AND MODEL FOR THE USER THEMSELVES
+        id for the specific user, and cards array
+
+    WRITE SCHEMA AND MODEL FOR USERS CARDS
+        name, details for card and quntity
+
+    THESE GO INSIDE OF THE ACTUAL CARD INFO COMMAND AS BUTTONS THE USER CAN SEE
+        darken the remove button if the user does not have the card currently in there collection.
+
+        WRITE COMMAND TO ALLOW USER TO ADD NEW CARD TO COLLECTION
+            make this a seperate function, check usersID number on discord, and find user in DB with the same ID then push the card to the users card collection array.
+                user.cards.filter(cards.card.name === name => cards.card.quantity += 1)
+            when the user clicks on either of the buttons provided send a new message that is ephemerel that states how many of that card the have left in the collection.
+
+        WRITE COMMAND TO ALLOW USER TO REMOVE CARD FROM COLLECTION
+
+    WRITE COMMAND TO SHOW USER FULL COLLECTION
+        have a button that allows a user to export the cards as txt for importing purposes on the eventually made website
+        
+
+*/
 
 // TODO LONG TERM GOALS
 
