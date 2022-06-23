235541d13bc0263bb8f0e16b521e3d95498e58ae
Crowe <81550296+Crowe7@users.noreply.github.com>
2022-06-23 10:15:20 -0500

    updated TODOs

--- src/index.ts
+++ src/index.ts
@@ -22,19 +22,12 @@ client.login(TOKEN);
 
 /*
 
-
-    THESE GO INSIDE OF THE ACTUAL CARD INFO COMMAND AS BUTTONS THE USER CAN SEE
-        darken the remove button if the user does not have the card currently in there collection.
-
-        WRITE COMMAND TO ALLOW USER TO ADD NEW CARD TO COLLECTION
-            make this a seperate function, check usersID number on discord, and find user in DB with the same ID then push the card to the users card collection array.
-                user.cards.filter(cards.card.name === name => cards.card.quantity += 1)
-            when the user clicks on either of the buttons provided send a new message that is ephemerel that states how many of that card the have left in the collection.
-
-        WRITE COMMAND TO ALLOW USER TO REMOVE CARD FROM COLLECTION
-
     WRITE COMMAND TO SHOW USER FULL COLLECTION
         have a button that allows a user to export the cards as txt for importing purposes on the eventually made website
+
+    WRITE COMMAND TO ALLOW USER TO IMPUT BULK COLLECTION DATA
+
+    WRITE COMMAND TO ALLOW USER TO REMOVE BULK COLLECTION DATA
 
 
 */
@@ -43,13 +36,10 @@ client.login(TOKEN);
 
 /*
 
-    WRITE COMMAND TO ALLOW USER TO ADD NEW CARD TO COLLECTION
 
     WRITE COMMAND TO ALLOW USER TO VIEW FULL COLLECTION
 
     WRITE COMMAND THAT ALLOWS USER TO EXPORT COLLECTION IN A WAY THAT THEY CAN IMPORT ONTO OTHER WEBSITES
-
-    WRITE COMMAND TO ALLOW REMOVING A CARD FROM COLLECTION
 
     ??? ALLOW USER TO SAVE DECKS AND GET CERTAIN STATS FROM DECK EG: AVRAGE CMC, COLORS, TYPES, ETC ???
 
