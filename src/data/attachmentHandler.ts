6c61f203bbf395f68d4d3cef1a7c5e0ca7180800
Crowe7 <jacobocrowe@gmail.com>
2022-08-28 01:40:56 -0500

    fixed bug with large files slowing the bot down

--- src/data/attachmentHandler.ts
+++ src/data/attachmentHandler.ts
@@ -1,10 +1,13 @@
 import { Attachment } from "discord.js";
+import { statSync } from "fs";
+// max file size
+const MAX_FILE_SIZE = 2000;
+
 
 export const attachmentHandler = async (attachment: Attachment) => {
-
     let attachmentType = attachment.attachment.toString().slice(-3);
 
-    if( attachmentType !== "txt") {
+    if( attachmentType !== "txt" || attachment.size < MAX_FILE_SIZE) {
         return
     }
 
